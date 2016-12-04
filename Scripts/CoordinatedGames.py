7af22c22ce652361f8525ebcb0f90b15844be1ea
Ismael Mercado <mebi90@hotmail.com>
2016-12-03 20:28:54 -0700

    Update CoordinatedGames.py

--- Scripts/CoordinatedGames.py
+++ Scripts/CoordinatedGames.py
@@ -13,7 +13,10 @@ def normalize(df):
     for feature_name in df.columns:
         max_value = df[feature_name].max()
         min_value = df[feature_name].min()
-        result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+        if(max_value != min_value):
+            result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+        else:
+            result[feature_name] = 1
     return result
 
 def define_player(player_vector, player_number):
